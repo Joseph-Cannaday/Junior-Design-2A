73deeaef0235ac5c7f0506fc1f143cefc7cc553c
Joseph-Cannaday <jcan315@icloud.com>
2023-03-11 13:31:23 -0500

    Connect:Adapted Connect to work with software architecture;main: Added generic process for startPlay

--- Connect.py
+++ Connect.py
@@ -7,87 +7,77 @@ import json
 import time
 
 # Global Constants
-DIRECTOR_HOST = "127.0.0.1"
-DIRECTOR_PORT = 3333
-
-CONNECTION_RETRY = 5          # Number of Tries before failrue
+DIRECTOR_HOST = "107.23.238.206"
+DIRECTOR_PORT = 3336
+CONNECTION_RETRY = 5          # Number of Tries before failure
 CONNECTION_RETRY_DELAY = 5000 # Milliseconds
-STATUS = 'CONNECTING'         # System Status (CONNECTING, CONNECTED, RUNNING, FAILED)
 ROBOT_ID = 'Snapdragonfly'
 
-# Global Variables
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP + IPv4
+class Connect:
+  def __init__(self):
+    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP + IPv4
+    self.self.status = 'CONNECTING'  # System self.status (CONNECTING, CONNECTED, RUNNING, FAILED)
 
-# Initialization Procedure
-def main():
-  connect(DIRECTOR_HOST, DIRECTOR_PORT)
-  if (STATUS != 'CONNECTED'): return print("Failed connection, exiting")
+  # Perform initial connection and identification procedure
+  def connect(self, host, port):
+    self.s.connect((host, port))
 
-  try:
-    while True:
-      decode_message(s.recv(1024).decode())
-  except socket.error:
-    print("[CLIENT] Failed...", )
-    s.close()
+    # Perform identification procedure
+    message = encode_json({ "cmd": "IDENTIFY", "data": ROBOT_ID })
+    self.s.send(message)
+    
+    # Verify identification was successful
+    callback = json.loads(self.s.recv(1024).decode())
+    if (callback['cmd'] == 'IDENTIFY' and callback['data'] == True):
+      print("[CLIENT] Connection successful")
+      self.status = 'CONNECTED'
+    
+    # Identification was unsuccessful
+    print("[CLIENT] Connection failed:", callback['data'])
+    self.status = 'FAILED'
 
-# Perform initial connection and identification procedure
-def connect(host, port):
-  global STATUS
-
-  s.connect((host, port))
-
-  # Perform identification procedure
-  message = encode_json({ "cmd": "IDENTIFY", "data": ROBOT_ID })
-  s.send(message)
-  
-  # Verify identification was successful
-  callback = json.loads(s.recv(1024).decode())
-  if (callback['cmd'] == 'IDENTIFY' and callback['data'] == True):
-    print("[CLIENT] Connection successful")
-    STATUS = 'CONNECTED'
-    return
-  
-  # Identification was unsuccessful
-  print("[CLIENT] Connection failed:", callback['data'])
-  STATUS = 'FAILED'
-
-# Central message decoder. Decodes the JSON message and performs actions
-# based on the message.
-def decode_message(message):
-  payload = json.loads(message)
-  
-  if payload['cmd'] == 'LINE':
+  # Central message decoder. Decodes the JSON message and performs actions
+  # based on the message.
+  def decode_message(self, message):
+    payload = json.loads(message)
+    
+    if payload['cmd'] == 'LINE':
     # Line given by the server once presentation starts and it is your turn
-    perform(payload['data'])
+      return payload['data']
 
     # Server signal that the presentation is complete
-  elif payload['cmd'] == 'PRESENTATION_COMPLETE':
-    print("[CLIENT] Presentation is complete")
-    
+    elif payload['cmd'] == 'PRESENTATION_COMPLETE':
+      print("[CLIENT] Presentation is complete")
+      return ''
+      
     # All lines for a given robot. Given by the server during initial connection
     # when running in echo mode
-  elif payload['cmd'] == 'LINES':
-    print("[CLIENT] Got lines:", payload['data'])
+    elif payload['cmd'] =='LINES':
+      print("[CLIENT] Got lines:", payload['data'])
+      return ''
 
     # When the server is running in echo mode, all messages sent to it
-    # will be returned to oring
-  elif payload['cmd'] == 'ECHO':
-    print('[CLIENT] Got echo from server:', payload['data'])
+    # will be returned to origin
+    elif payload['cmd'] == 'ECHO':
+      print('[CLIENT] Got echo from server:', payload['data'])
+      return ''
 
     # If the command does not match any of the commands above, move on
-  else:
+    else:
       print('[CLIENT] Got undefined response from server:', payload['cmd'])
+      return ''
 
-# Handles a given line as handed by the server. In this implementation,
-# it waits 5 seconds and sends back LINE_COMPLETE to the server, which
-# allows the server to move on with the presentation
-def perform(line):
-  print("[CLIENT] Got a line:", line)
-  time.sleep(5)
-  s.send(encode_json({ "cmd": "LINE_COMPLETE" }))
-
-def encode_json(obj):
-  return json.dumps(obj, separators=(',', ':')).encode()
+  def encode_json(self, obj):
+    return json.dumps(obj, separators=(',', ':')).encode()
 
 if __name__ == '__main__':
-    main()
+    c1 = Connect()
+    c1.connect(DIRECTOR_HOST, DIRECTOR_PORT)
+    
+    if (c1.status != 'CONNECTED'): return print("Failed connection, exiting")
+    try:
+      while True:
+        c1.decode_message(c1.s.recv(1024).decode())
+    except socket.error:
+      print("[CLIENT] Failed...", )
+      c1.s.close()
